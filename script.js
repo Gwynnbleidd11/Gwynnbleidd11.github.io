4b704bb8e41f814aacf95ae64537ab1cc0286620
Gwynnbleidd11 <140753432+Gwynnbleidd11@users.noreply.github.com>
2024-01-03 10:17:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://kodilla-rest-api.patrykkrzyzak.repl.co/v1/tasks';
+  var apiRoot = 'https://kodilla-rest-api.patrykkrzyzak.repl.com/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
